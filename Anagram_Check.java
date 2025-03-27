fe5f1f248581e8d28e6ad81d454fdfab2b221471
venkat-seenivasan <venkateshsrinivasan2003@gmail.com>
2025-03-27 18:51:14 +0530

    add using for loop

--- Anagram_Check.java
+++ Anagram_Check.java
@@ -10,13 +10,23 @@ public class Anagram_Check
             System.out.println(false);
             return;
         }
+        char ans1[]=new char[a.length()];
+        char ans2[]=new char[a.length()];
+        for (int i=0;i<a.length();i++){
+            ans1[i]=a.charAt(i);
+            ans2[i]=b.charAt(i);
+        }
+        Arrays.sort(ans1);
+        Arrays.sort(ans2);
         
-        char[] arr1 = a.toCharArray();
-        char[] arr2 = b.toCharArray();
+        // char[] arr1 = a.toCharArray();
+        // char[] arr2 = b.toCharArray();
         
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
         
-        System.out.println(Arrays.equals(arr1, arr2));
+        // System.out.println(Arrays.equals(arr1, arr2));
+        System.out.println(Arrays.equals(ans1, ans2));
+
     }
 }
